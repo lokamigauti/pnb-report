ec994acb5e8bc2b5e90a9fe234d9b744494c1972
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-03-17 18:05:29 +0000

    Make import_total_plays for Anchor files

--- data_import.py
+++ data_import.py
@@ -9,6 +9,7 @@ them. Some meta-data (e.g. the podcast name) is in the file name itself, so the 
 import numpy as np
 import pandas as pd
 from types import * # types is safe for import *
+from datetime import datetime
 
 DATA_FOLDER = 'G:/My Drive/Podcast/PNB/data/'
 OUTPUT_FOLDER = DATA_FOLDER + 'formatted/'
@@ -22,10 +23,23 @@ def import_total_plays(path, source):
     :return: pandas dataframe with columns 'time' and 'plays', with '1D' interval.
     """
 
-    sources_list = ['Anchor']
+    sources_list = ['test', 'Anchor']
 
-    assert type(path) is StringType, 'path must be a string: %r' % path
-    assert type(source) is StringType, 'source must be a string: %r' % source
+    assert type(path) is str, 'path must be a string: %r' % path
+    assert type(source) is str, 'source must be a string: %r' % source
     assert source in sources_list, 'Source %r not implemented. Make sure the spelling is correct.' % source
 
+    if source is 'test':
+        total_plays = None
+
+    if source is 'Anchor':
+        # by default, %H is 00-23, however we do not need the time info as it is daily.
+        anchor_date_parser = lambda x: datetime.strptime(x, '%m/%d/%Y 24:00:00')
+        total_plays = pd.read_csv(path, names=['time', 'plays'], header=0, parse_dates=['time'],
+                                  date_parser=anchor_date_parser)
+
+    return total_plays
+
+
 if __name__ == '__main__':
+    total_plays = import_total_plays(DATA_FOLDER + 'Dropzilla_TotalPlays_all-time.csv', 'Anchor')
